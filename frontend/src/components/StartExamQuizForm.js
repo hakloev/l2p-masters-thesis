475805cbdc6337def8bfdb92700d7321f96b3f2f
Fredrik Christoffer Berg <fredrcb@stud.ntnu.no>
2016-10-25 14:35:11 +0200

    Changed button text

--- frontend/src/components/StartExamQuizForm.js
+++ frontend/src/components/StartExamQuizForm.js
@@ -18,7 +18,7 @@ const StartExamQuizForm = ({ onSubmit }) => {
       <div className="col s12">
         <h3>Exam Questions</h3>
         <p>These tasks are of varying difficulty and are meant as practice prior to the exam.</p>
-        <button type="submit" onClick={onSubmitClick} className="btn waves-effect waves-light blue darken-4">Start!</button>
+        <button type="submit" onClick={onSubmitClick} className="btn waves-effect waves-light blue darken-4">Start exam practice</button>
       </div>
     </div>
   );
