f834988039d5b6d3c6653fad290d048730de9c32
Håkon Ødegård Løvdal <me@hakloev.no>
2016-11-07 10:41:37 +0100

    Fix navbar design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,34 +1,51 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 
-const Navbar = ({ isAuthenticated, onLogoutClick }) => {
+const menuItems = (menuClass, menuId, { isAuthenticated, onLogoutClick }) => {
   return (
-    <div className="navbar-fixed">
-      <nav>
-        <div className="nav-wrapper blue">
-          <Link to="/" className="brand-logo">LearnPython2</Link>
-          <ul className="right hide-on-med-and-down">
-            <li><Link to="/" className="mdl-navigation__link">Home</Link></li>
-            {!isAuthenticated &&
-              <li><Link to="/register" className="mdl-navigation__link">Register</Link></li>
-            }
-            {isAuthenticated &&
-              <li><Link to="/report" className="mdl-navigation__link">Report an Issue</Link></li>
-            }
-            {isAuthenticated
-              ? <li><Link className="mdl-navigation__link" onClick={e => onLogoutClick(e)}>Logout</Link></li>
-              : <li><Link to="/login" className="mdl-navigation__link">Login</Link></li>
-            }
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <ul className={menuClass} id={menuId}>
+      <li><Link to="/" className="mdl-navigation__link">Home</Link></li>
+      {!isAuthenticated &&
+        <li><Link to="/register" className="mdl-navigation__link">Register</Link></li>
+      }
+      {isAuthenticated &&
+        <li><Link to="/report" className="mdl-navigation__link">Report an Issue</Link></li>
+      }
+      {isAuthenticated
+        ? <li><Link className="mdl-navigation__link" onClick={e => onLogoutClick(e)}>Logout</Link></li>
+        : <li><Link to="/login" className="mdl-navigation__link">Login</Link></li>
+      }
+    </ul>
   );
 };
 
+class Navbar extends Component {
+
+  componentDidMount() {
+    $('.button-collapse').sideNav();
+  }
+
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="blue">
+          <div className="nav-wrapper">
+            <Link to="/" className="brand-logo">LearnPython2</Link>
+            <a href="#" data-activates="mobile-nav" className="button-collapse"><i className="material-icons">menu</i></a>
+            {menuItems('right hide-on-med-and-down', '', this.props)}
+            {menuItems('side-nav', 'mobile-nav', this.props)}
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+/* eslint-disable */
 Navbar.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
   onLogoutClick: PropTypes.func.isRequired,
 };
+/* eslint-enable */
 
 export default Navbar;
