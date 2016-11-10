f8b4fb9ee0e4d1bc0f2738143047083e3d43219d
Håkon Ødegård Løvdal <me@hakloev.no>
2016-11-10 11:36:17 +0100

    Address issue #10, and add production key

--- frontend/src/common/constants.js
+++ frontend/src/common/constants.js
@@ -1,6 +1,6 @@
 const TOKEN_IDENTIFIER = 'id_token';
 const CODE_STRIP_REGEX = new RegExp(/^\s+|\n+|\r+|\s+$/, 'g');
-const ANALYTICS_KEY = 'UA-86402640-1';
+const ANALYTICS_KEY = process.env.NODE_ENV === 'production' ? 'UA-86402640-2' : 'UA-86402640-1';
 
 export {
   TOKEN_IDENTIFIER,
