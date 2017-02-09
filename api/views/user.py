d4d8659e9d7f1612a0ecfe968542fe79b3225d55
Håkon Ødegård Løvdal <me@hakloev.no>
2017-02-09 11:15:19 +0100

    Remove redundant code from user.py

--- api/views/user.py
+++ api/views/user.py
@@ -38,9 +38,6 @@ class RegistrationView(generics.CreateAPIView):
     permission_classes = ()
     authentication_classes = ()
 
-    permission_classes = ()
-    authentication_classes = ()
-
     def post(self, request):
         log.debug('User creation requested')
         serializer = RegistrationSerializer(data=request.data)
